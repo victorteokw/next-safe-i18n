22d660f3557d5c37141dd1f1f2ed1824bf0e33a1
Victor Teo <yeannylam@gmail.com>
2026-09-17 14:03:30 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,2 @@
 export type { default as I18nConfig } from "./config"
-export { createI18n, type I18n, type Params } from "./i18n"
+export { createI18n, type I18n, type Params, type Props } from "./i18n"
